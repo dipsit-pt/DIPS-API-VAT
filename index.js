1ac272d622107575fa11a8bfb80630d52cf19b99
“Alexandre <alexandre.graca@dipsit.pt>
2024-11-28 23:37:03 +0000

    fix bug on node modules importing

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import router from "./routes";
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: 'SERVER-API-VAT' });
+});
 
 export { router as rVAT };
--- routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const router = Router();
-
-router.get('/', (req, res) => {
-    res.json({ message: 'SERVER-API-VAT' });
-});
-
-export default router;
